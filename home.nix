96f22c57e0899d7702eed2836b4d9fdae0d419ab
kuritsu <rodrigoalexccorreia@gmail.com>
2025-05-05 05:50:14 +0100

    updated dots

--- home.nix
+++ home.nix
@@ -71,4 +71,7 @@
 
   home.file.".config/wofi/config".source = ./.config/wofi/config;
   home.file.".config/wofi/style.css".source = ./.config/wofi/style.css;
+
+  home.file.".config/.zshrc" =
+    pkgs.lib.mkIf (standalone) { source = ./.zshrc; };
 }
