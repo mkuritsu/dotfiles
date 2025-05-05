dfad2e3da5c87916ca3566b1a54746e3a321b1bc
kuritsu <rodrigoalexccorreia@gmail.com>
2025-05-05 03:20:29 +0100

    updated nix config

--- flake.nix
+++ flake.nix
@@ -1,11 +1,35 @@
 {
-  description = "Kuritsu dotfiles";
+  description = "Kuritsu's dotfiles";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
-    dotfiles = import ./dotfiles.nix { pkgs = nixpkgs; };
-  };
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in {
+      home = ./home.nix;
+
+      homeConfigurations."kuritsu" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = {
+          inherit inputs;
+          standalone = true;
+        };
+        modules = [ ./home.nix ];
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = [ home-manager.packages.${system}.home-manager ];
+      };
+    };
 }
