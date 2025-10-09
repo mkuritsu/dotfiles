8f93fcf00a33d8b85b3cd5d456eb63c6d72feaf9
mkuritsu <mkuritsuu@gmail.com>
2025-10-09 04:37:55 +0100

    make find include symlinks

--- dots/fish/functions/cd_fzf.fish
+++ dots/fish/functions/cd_fzf.fish
@@ -1,5 +1,5 @@
 function cd_fzf
-    set dir $(find ~/ ~/Dev . -mindepth 1 -maxdepth 1 -type d | fzf)
+    set dir $(find ~/ ~/Dev . -mindepth 1 -maxdepth 1 \( -type d -o -type l \) | fzf)
     if test -n "$dir"
         cd $dir
         commandline -f repaint
