ea842eea00b74929c9b814a88bdb31408cc2318e
mkuritsu <mkuritsuu@gmail.com>
2025-10-11 18:51:39 +0100

    go back to kitty

--- modules/xdg.nix
+++ modules/xdg.nix
@@ -9,7 +9,7 @@
       enable = true;
       settings = {
         GNOME = [ "ghostty.desktop" ];
-        default = [ "ghostty.desktop" ];
+        default = [ "kitty.desktop" ];
       };
     };
   };
