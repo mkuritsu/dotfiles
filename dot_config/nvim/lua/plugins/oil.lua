76df80630b59a421e173fc8ba6e1d79243099984
mkuritsu <mkuritsuu@gmail.com>
2026-04-10 23:41:12 +0100

    env vars update, WMs update, noctalia update

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -2,8 +2,21 @@ return {
 	"stevearc/oil.nvim",
 	lazy = false,
 	opts = {
+		float = {
+			border = "rounded",
+			padding = 2,
+		},
 		view_options = {
 			show_hidden = true,
+		},
+	},
+	keys = {
+		{
+			"<leader>o",
+			function()
+				require("oil").open_float()
+			end,
+			desc = "Open Oil floating",
 		},
 	},
 }
